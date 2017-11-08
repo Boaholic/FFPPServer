6bbaeaff130227405d79c065bc4acb5c034481f5
Skyler Schvaneveldt <boaholic@gmx.com>
2017-11-08 11:37:13 -0700

    Original commit

---
+++ FFPPServer/ClientPlayer.cs
@@ -0,0 +1,68 @@
+﻿using log4net;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FFPPServer
+{
+    public class clientPlayer
+    {
+        public string playerName { get; set; }
+        public static readonly ILog Logger = LogManager.GetLogger(typeof(serverMessage));
+        public string A_Number { get; set; }
+        public IPAddress _ServerAddress { get; set; }
+        public UdpClient MyUdpClient { get; set; }
+
+        public IPEndPoint myEndPoint;
+        public IPEndPoint _ServerEndPoint;
+        public static System.Timers.Timer Controller;
+        public int Port { get; set; }
+        // System.Timers.TImer, db heartbeat, in score , s answer 
+
+        public int Score { get; set; }
+
+        public clientPlayer(serverMessage initialClientMessage)
+        {
+            //extract the player name
+            //set the value of variable playerName
+            //to the input value
+        }
+        public bool hasJoinedGame
+        {
+            get
+            {
+                return hasJoinedGame;
+            }
+            set
+            {
+                hasJoinedGame = value;
+            }
+        }
+        public bool playerHasRequestedJoin
+        {
+            get
+            {
+                return playerHasRequestedJoin;
+            }
+            set
+            {
+                playerHasRequestedJoin = value;
+            }
+        }
+        public bool playerHasRequestedLeave
+        {
+            get
+            {
+                return playerHasRequestedLeave;
+            }
+            set
+            {
+                playerHasRequestedLeave = value;
+            }
+        }
+        // public serverMessage SendIsReady()
+        //{
+        //create an isReady message
+        //send an isReady message
+        // }
+    }
+}
---
+++ FFPPServer/lobbyServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFPPServer
+{
+    public class lobbyServer
+    {
+        public clientPlayer[] totalAssociatedPlayers;
+        public lobbyGame[] gamesInLobby;
+        public void newLobbyPlayer(clientPlayer newPlayer)
+        {
+            foreach (clientPlayer p in totalAssociatedPlayers)
+            {
+                if (newPlayer == p)
+                {
+                    return; //The player already exists
+                }
+            }
+            totalAssociatedPlayers.SetValue(newPlayer, totalAssociatedPlayers.GetUpperBound(1) + 1);
+        }
+
+        public lobbyServer()
+        {
+            totalAssociatedPlayers = null;
+            //Create two new instances of lobbygame
+            lobbyGame firstGame = new lobbyGame();
+            lobbyGame secondGame = new lobbyGame();
+            gamesInLobby.SetValue(firstGame, gamesInLobby.GetUpperBound(1) + 1);
+            gamesInLobby.SetValue(secondGame, gamesInLobby.GetUpperBound(1) + 1);
+        }
+    }
+}
