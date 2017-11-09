e6a66b2972a3b2364284d4e17194648c33c8aea3
meganjo167 <megan.jo167@gmail.com>
2017-11-09 14:20:13 -0700

    Added encoding decoding test

---
+++ MessageTesting/MessageTesting.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FFPPServer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMessageCoding
+{
+    [TestClass]
+    public class MessageTesting
+    {
+        [TestMethod]
+        public void TestingEncodingDecoding()
+        {
+            ReadWrite _messageReadWrite = new ReadWrite();
+            Message TestMessage = new Message(Message.messageType.JOIN, "PlayerName");
+            byte[] EncodedMessage = _messageReadWrite.EncodeMessage(TestMessage);
+            _messageReadWrite.DecodeMessage(EncodedMessage);
+
+            Assert.AreEqual(TestMessage.thisMessageType, _messageReadWrite.targetMessage.thisMessageType);
+            Assert.AreEqual(TestMessage.messageBody, _messageReadWrite.targetMessage.messageBody);
+        }
+    }
+}
