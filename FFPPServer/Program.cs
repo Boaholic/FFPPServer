b2acbf567a515349c0b93ab1dc92651af5a3de64
Joshua Wilding <joshua.wilding@aggiemail.usu.edu>
2017-11-15 13:19:31 -0700

    Server now builds and runs. Logging does not work yet.

--- FFPPServer/Program.cs
+++ FFPPServer/Program.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using log4net;
+using log4net.Config;
 
 namespace FFPPServer
 {
     class Program
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
         static void Main(string[] args)
         {
+            XmlConfigurator.Configure(new System.IO.FileInfo(args[0]));   
+            LobbyServer mainLobby = new LobbyServer();
         }
     }
 }
