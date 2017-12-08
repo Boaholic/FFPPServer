f9347c27c055161216029d0ddd6c6ed4e62afe4d
meganjo167 <megan.jo167@gmail.com>
2017-12-08 12:08:54 -0700

    Finally got communications working

--- FFPPServer/Program.cs
+++ FFPPServer/Program.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using log4net;
 using log4net.Config;
+using System.Threading;
 
 namespace FFPPServer
 {
@@ -16,6 +17,14 @@ namespace FFPPServer
            // XmlConfigurator.Configure(new System.IO.FileInfo("./App.config"));   
             //BasicConfigurator.Configure();
             LobbyServer mainLobby = new LobbyServer();
+            DataProccessor dataProccessor = new DataProccessor
+            {
+                Lobby = mainLobby
+            };
+
+            Thread ProccessorThread = new Thread(new ThreadStart(dataProccessor.Start));
+            ProccessorThread.Start();
+
             log.Info("Server Started!");
             Console.ReadLine();
         }
